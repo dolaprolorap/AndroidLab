aa0d864e92c4da31c3b862face11cd16ac35839c
dolaprolorap <dolaprolorapff@gmail.com>
2024-06-18 15:16:46 +1000

    Add TotalCalories view

---
+++ app/src/main/java/com/example/forandroid/TotalCaloriesView.kt
@@ -0,0 +1,75 @@
+package com.example.forandroid
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.LinearLayout
+import android.widget.RelativeLayout
+import com.example.forandroid.databinding.TotalCaloriesViewBinding
+
+class TotalCaloriesView : RelativeLayout {
+    var attributeSet: AttributeSet? = null
+    val productList: MutableList<ProductData> = mutableListOf()
+    val onAddProductBtnClicked: Event0 = Event0()
+    val onMakeDietBtnClicked: Event0 = Event0()
+
+    constructor(context: Context, attributeSet: AttributeSet) : super(context, attributeSet) {
+        this.attributeSet = attributeSet
+    }
+
+    constructor(context: Context) : super(context)
+
+    private val binding =
+        TotalCaloriesViewBinding.inflate(LayoutInflater.from(context), this, false)
+
+    init {
+        addView(binding.root)
+        updateView()
+
+        val addProductBtn : Button = findViewById(R.id.addProductBtn)
+        val makeDietBtn: Button = findViewById(R.id.makeDietBtn)
+
+        addProductBtn.setOnClickListener {
+            onAddProductBtnClicked()
+        }
+
+        makeDietBtn.setOnClickListener {
+            onMakeDietBtnClicked()
+        }
+    }
+
+    fun addProduct(prod: ProductData) {
+        productList.add(prod)
+        updateView()
+    }
+
+    fun removeProduct(prod: ProductData) {
+        productList.remove(prod)
+        updateView()
+    }
+
+    fun hasProduct(prod: ProductData) : Boolean {
+        return productList.contains(prod)
+    }
+
+    fun updateView() {
+        var totalCalories = 0f
+        var totalProteins = 0f
+        var totalFats = 0f
+        var totalCarbohydrates = 0f
+
+        for (product in productList) {
+            totalCalories += product.calories
+            totalProteins += product.proteins
+            totalFats += product.fats
+            totalCarbohydrates += product.carbohydrates
+        }
+
+        binding.totalCaloriesTV.text = "Calories:\n ${String.format("%.1f", totalCalories)}"
+        binding.totalProteinsTV.text = "Proteins: ${String.format("%.1f", totalProteins)}"
+        binding.totalFatsTV.text = "Fats: ${String.format("%.1f", totalFats)}"
+        binding.totalCarbohydratesTV.text = "Carbohydrates: ${String.format("%.1f", totalCarbohydrates)}"
+    }
+}
